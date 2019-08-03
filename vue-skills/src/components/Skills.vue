462671f63d0ea3471c5407fc7c06acef4cfcf36f
unknown <nicholas.harvey1914@gmail.com>
2019-08-03 17:41:11 -0400

    pushing info

---
+++ vue-skills/src/components/Skills.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+<div class='holder'></div>
+<ul>
+  <li v-for="(data, index) in skills" :key='index'> {{ index }}. {{data.skill }}</li>
+</ul>
+
+<p>These are the skills that you posess</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+data() {
+  return{
+    skills: [
+      {'skill':'Vue.js'},
+      {'skill':'Front End Developer'}
+    ],
+    bgColor: 'yellow',
+    bgWidth: '100%',
+    bgHeight: '30px',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+</style>
